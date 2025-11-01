79e073e55c0c23aba9759a50bdf3b670e96e6172
alexandrshoobeen <alexandrshoobeen@gmail.com>
2025-11-02 00:40:45 +0300

    create folders

---
+++ house/house.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var hero_mouse_scene = preload("res://hero_mouse.tscn")
+	var hero_mouse = hero_mouse_scene.instantiate()
+	add_child(hero_mouse)
