7b861367a5b61d901143bdfd47b5ae4a290492ad
lkatyniuu <lkatyniuu@gmail.com>
2025-11-02 04:04:36 +0300

    change dnd behavior

--- room/InventoryItem.gd
+++ room/InventoryItem.gd
@@ -3,17 +3,23 @@ extends TextureRect
 
 @export var data: ItemData
 
+signal drag_started(item_data: ItemData)
+
 func init(d: ItemData) -> void:
 	data = d
 	
 func _ready() -> void:
+	if data == null:
+		return  # Exit if no data assigned yet
 	expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 	texture = data.texture
 	tooltip_text = "%s\n%s" % [data.name, data.description]
 	
 func _get_drag_data(at_position: Vector2) -> Variant:
-	set_drag_preview(make_drag_preview(at_position))
+	#set_drag_preview(make_drag_preview(at_position))
+	print("DRAG STARTED")
+	emit_signal("drag_started", data)
 	return self
 	
 func make_drag_preview(at_position: Vector2):
