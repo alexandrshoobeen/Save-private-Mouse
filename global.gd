910c00e447d071be05d2205c20c9c041b2efdd9c
lkatyniuu <lkatyniuu@gmail.com>
2025-11-04 14:48:52 +0300

    Collect items (#10)
    * collect items
    
    * hide items

--- global.gd
+++ global.gd
@@ -7,6 +7,8 @@ var inventory_data: Array = []
 # Placed objects: array of dictionaries {item_name, item_id, cell, size, texture_path}
 var placed_objects: Array = []
 
+var current_inv_item: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- room/gui.gd
+++ room/gui.gd
@@ -5,19 +5,65 @@ extends CanvasLayer
 var InvSize = 3
 var itemsLoad = ["res://room/ItemsResources/Spool.tres","res://room/ItemsResources/Candy.tres", "res://room/ItemsResources/MatchBox.tres"]
 # Called when the node enters the scene tree for the first time.
+	
+func is_item_not_placed(item_name: String) -> bool:
+	# Check if the item_name exists in any placed object
+	for placed_obj in Global.placed_objects:
+		if item_name == placed_obj.item_name:
+			return false
+	return true
+
+
 func _ready() -> void:
+	# Create a Label for tooltips
+	tooltip_label = Label.new()
+	tooltip_label.visible = false
+	add_child(tooltip_label)
+	print(Global.inventory_data, "Inventory data")
+	
 	for i in InvSize:
 		var slot := InventorySlot.new()
 		slot.init(ItemData.Type.MAIN, Vector2(64, 64))
 		%Inv.add_child(slot)
 		pass
+		
+	
+	for i in range(itemsLoad.size()):
+		var res = load(itemsLoad[i])
+		for inv in range(Global.inventory_data.size()):
+			if(Global.placed_objects.size() > 0):
+				for obj in range(Global.placed_objects.size()):
+					print(Global.placed_objects, "placed")
+					if res.name == Global.inventory_data[inv].name and is_item_not_placed(res.name):
+						var item = InventoryItem.new()
+						item.init(res)
+						%Inv.get_child(i).add_child(item)
+						## In Placement.gd
+						if res.name != "Candy":
+							item.connect("drag_started", Callable(self, "_on_drag_started"))
+						else:
+							item.connect("gui_input", Callable(self, "_on_candy_click"))
+							#item.connect("mouse_entered", Callable(self, "_on_candy_hovered"))
+							#item.connect("mouse_exited", Callable(self, "_on_candy_unhovered"))
+			else:
+				if res.name == Global.inventory_data[inv].name:
+					var item = InventoryItem.new()
+					item.init(res)
+					%Inv.get_child(i).add_child(item)
+					## In Placement.gd
+					if res.name != "Candy":
+						item.connect("drag_started", Callable(self, "_on_drag_started"))
+					else:
+						item.connect("gui_input", Callable(self, "_on_candy_click"))
+						#item.connect("mouse_entered", Callable(self, "_on_candy_hovered"))
+						#item.connect("mouse_exited", Callable(self, "_on_candy_unhovered"))
 
-	for i in itemsLoad.size():
-		var item = InventoryItem.new()
-		item.init(load(itemsLoad[i]))
-		%Inv.get_child(i).add_child(item)
-		# In Placement.gd
-		item.connect("drag_started", Callable(self, "_on_drag_started"))
+	#for i in itemsLoad.size():
+		#var item = InventoryItem.new()
+		#item.init(load(itemsLoad[i]))
+		#%Inv.get_child(i).add_child(item)
+		### In Placement.gd
+		#item.connect("drag_started", Callable(self, "_on_drag_started"))
 	
 	# Create exit button
 	_create_exit_button()
@@ -58,3 +104,28 @@ func _on_drag_started(item_data: ItemData) -> void:
 	print("Drag started:", item_data.name)
 	if %BuildSystem.has_method("start_preview"):
 		%BuildSystem.start_preview(item_data)
+		
+func _on_candy_click(event) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if %BuildSystem.has_method("remove_item_from_inventory"):
+			%BuildSystem.remove_item_from_inventory("Candy")
+		Global.current_inv_item = "Candy"
+	
+func _on_candy_hovered():
+	# Use viewport mouse position for Control nodes
+	var mouse_pos = get_viewport().get_mouse_position()
+	show_tooltip("Yummy Candy! 🍬", mouse_pos)
+
+func _on_candy_unhovered():
+	hide_tooltip()
+	
+# In your main UI or CanvasLayer
+var tooltip_label: Label
+
+func show_tooltip(text: String, position: Vector2):
+	tooltip_label.text = text
+	tooltip_label.position = position
+	tooltip_label.visible = true
+
+func hide_tooltip():
+	tooltip_label.visible = false
